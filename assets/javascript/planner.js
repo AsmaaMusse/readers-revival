61b06f384b1b0ead6e733623208e4d28a1d652f5
Intisar Mohamed <Intisarm48@gmail.com>
2021-10-29 15:58:59 +0100

    myplanner page

---
+++ assets/javascript/planner.js
@@ -0,0 +1,7 @@
+//event listener to buttons that goes next month
+
+//event listener to buttons that goes previous month
+
+//render the book cards that were added on to my planner on the box
+
+// sort book card data into the months it was scheduled for
