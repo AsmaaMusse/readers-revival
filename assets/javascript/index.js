d2e6a99117fdcdbc73b2e18e5c8ea60c4f5a9a19
Darion Richards <darionrichards@outlook.com>
2021-10-29 12:27:35 +0100

    Successfully received book data for a specific search
    - Dynamic URL
    - Successfully tested different searches
    - Returns object related to book title searched

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -12,3 +12,13 @@ const hamburgerDropDown = () => {
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(toggleActive)
 };
+
+const getBookData = async bookName => {
+    const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=${bookName}`;
+    // console.log(bookUrl)
+    const bookDataResponse = await fetch(bookUrl);
+    const bookData = await bookDataResponse.json();
+    // console.log(bookData.items)
+};
+
+getBookData("harry potter");
