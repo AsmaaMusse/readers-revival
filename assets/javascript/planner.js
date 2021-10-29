8452f6b6bb0d4cc523788d4a8febaa1328d3ac77
Intisar Mohamed <Intisarm48@gmail.com>
2021-10-29 18:03:03 +0100

    my planner page

--- assets/javascript/planner.js
+++ assets/javascript/planner.js
@@ -1,7 +1,34 @@
-//event listener to buttons that goes next month
+//declare months
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+//declare const
+const previous = document.getElementById("prev-btn");
+const next = document.getElementById("next");
 
-//event listener to buttons that goes previous month
+//add event listener
+previous.addEventListener("click", displayMonth);
+next.addEventListener("click", displayMonth);
+
+//display month function
+const displayMonth = function (previous, next) {
+  //if prev btn is clicked
+  if (previous)
+    //change the text of h1
+    document.querySelector(".month h1").innerHTML = "January";
+  //if next btn is clicked go forward
+};
 
 //render the book cards that were added on to my planner on the box
-
 // sort book card data into the months it was scheduled for
