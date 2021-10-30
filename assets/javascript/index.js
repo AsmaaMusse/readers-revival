a81cf4bf9612b8ba8c0549f8122e0e67522dde37
Asmaa <Asmaa99musse@gmail.com>
2021-10-30 12:50:57 +0100

    created LS for books

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,37 +1,53 @@
 $(document).ready(() => {
-    hamburgerDropDown();
+  hamburgerDropDown();
 });
 
 const hamburgerDropDown = () => {
-    const toggleActive = () => {
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-    }
+  const toggleActive = () => {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  };
 
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(toggleActive)
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(toggleActive);
 };
 
+// Get book card from API
 const getBookCardsData = (books) => {
-    const callback = (bookItem) => {
-        return {
-            title: bookItem.volumeInfo.title,
-            description: bookItem.volumeInfo.description,
-            img: bookItem.volumeInfo.imageLinks.smallThumbnail
-        }
+  const callback = (bookItem) => {
+    return {
+      title: bookItem.volumeInfo.title,
+      description: bookItem.volumeInfo.description,
+      img: bookItem.volumeInfo.imageLinks.smallThumbnail,
     };
-    return books.items.slice(0, 6).map(callback)
+  };
+  return books.items.slice(0, 6).map(callback);
 };
 
-const getBookData = async bookName => {
-    const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=${bookName}`;
-    const bookDataResponse = await fetch(bookUrl);
-    const bookData = await bookDataResponse.json();
+const getBookData = async (bookName) => {
+  const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=${bookName}`;
+  const bookDataResponse = await fetch(bookUrl);
+  const bookData = await bookDataResponse.json();
 
-    const bookCard = getBookCardsData(bookData)
+  const bookCard = getBookCardsData(bookData);
 
-    console.log(bookCard)
+  console.log(bookCard);
 };
 
 getBookData("harry potter");
+
+// Save books into Local Storage
+const setBooksInLS = function (bookName) {
+  // get books from LS
+  const book = JSON.parse(localStorage.getItem("recentBook")) ?? [];
+
+  // if book does not exist
+  if (!book.includes(bookName)) {
+    // insert bookName in book
+    book.push(bookName);
+
+    // set book in LS
+    localStorage.setItem("recentBook", JSON.stringify(book));
+  }
+};
